887c188a1f4c5ea35aa2016a8980ab5be2e0cb3e
FranciszekZielinski <franek.z.1231@gmail.com>
2023-01-22 17:49:01 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+
+setup(
+    name='homework_exercise',
+    version='0.1',
+    url='https://github.com/FranciszekZielinski/homework.git',
+    author='Franciszek Zieliński',
+    author_email='fz@gmail.com',
+    install_requires=['numpy'],
+
+)
